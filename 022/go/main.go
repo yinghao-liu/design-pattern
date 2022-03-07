a1c9a9517eb63654815bb589a3358bb772a7bffe
francis <yinghao_liu@163.com>
2022-03-07 09:46:40 +0800

    update templatepattern

--- 022/go/main.go
+++ 022/go/main.go
@@ -3,6 +3,9 @@ package main
 import "templatepattern/templatepattern"
 
 func main() {
-	var tt templatepattern.Concrete
+	var tt templatepattern.Template
+	var con templatepattern.Concrete
+	tt.Set(con)
+
 	tt.Template()
 }
--- 022/go/templatepattern/templatepattern.go
+++ 022/go/templatepattern/templatepattern.go
@@ -1,24 +1,66 @@
 package templatepattern
 
-// type Base struct {
-// }
+import "fmt"
 
-// func (b Base) Template() {
-// 	b.Step1()
-// 	b.Step2()
-// }
+/* type Base struct {
+}
 
-// func (b Base) Step1() {
-// 	fmt.Printf("base step 1\n")
-// }
-// func (b Base) Step2() {
-// 	fmt.Printf("base step 2\n")
-// }
+func (b Base) Template() {
+	b.Step1()
+	b.Step2()
+}
 
-// type Concrete struct {
-// 	Base
-// }
+func (b Base) Step1() {
+	fmt.Printf("base step 1\n")
+}
+func (b Base) Step2() {
+	fmt.Printf("base step 2\n")
+}
 
-// func (b Concrete) Step2() {
-// 	fmt.Printf("Concrete step 2\n")
-// }
+type Concrete struct {
+	Base
+}
+
+func (b Concrete) Step2() {
+	fmt.Printf("Concrete step 2\n")
+} */
+
+/*********************模板类*************************/
+type Template struct {
+	temp Templater
+}
+
+func (b *Template) Set(temp Templater) {
+	b.temp = temp
+}
+
+func (b Template) Template() {
+	b.temp.Step1()
+	b.temp.Step2()
+}
+
+/*********************模板步骤接口*************************/
+type Templater interface {
+	Step1()
+	Step2()
+}
+
+/*********************步骤基类*************************/
+type Base struct {
+}
+
+func (b Base) Step1() {
+	fmt.Printf("base step 1\n")
+}
+func (b Base) Step2() {
+	fmt.Printf("base step 2\n")
+}
+
+/*********************步骤具体类*************************/
+type Concrete struct {
+	Base
+}
+
+func (b Concrete) Step2() {
+	fmt.Printf("Concrete step 2\n")
+}
